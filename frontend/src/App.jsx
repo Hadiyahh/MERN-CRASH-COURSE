e7524283568ab77cbdc6482238ed647c6f580bcf
Hadiyah Arif <arifh@uwindsor.ca>
2025-08-30 22:27:40 -0400

    Describe your changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,20 @@
-// import { Box, Button } from '@chakra-ui/react'
-// import { Route, Routes } from 'react-router-dom'
-// import CreatePage from './pages/CreatePage'
-// import HomePage from './pages/HomePage'
-// import Navbar from './components/Navbar'
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
 
-// function App() {
-//   return (
-//     <Box minH={"100vh"}>
-//       <Navbar />
-//       <Button colorScheme="blue">Click me</Button>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/create" element={<CreatePage />} />
-//       </Routes>
-//     </Box>
-//   )
-// }
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
-import { Box, Button } from "@chakra-ui/react";
-import { Routes, Route, Link } from "react-router-dom";
-
-function Home()   { return <Box p={6}>Home</Box>; }
-function Create() { return <Box p={6}>Create</Box>; }
-
-export default function App() {
-  return (
-    <Box p={24}>
-      <Box mb={4}>
-        <Link to="/" style={{ marginRight: 12 }}>Home</Link>
-        <Link to="/create">Create</Link>
-      </Box>
-      <Button colorScheme="blue" mb={4}>Router v6 OK</Button>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
-    </Box>
-  );
+function App() {
+	return (
+		<Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/create' element={<CreatePage />} />
+			</Routes>
+		</Box>
+	);
 }
+
+export default App;
